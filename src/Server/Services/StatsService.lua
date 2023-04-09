e307fe80221e9823ef88df9b038b8275b9fee33a
Max Bacon <maxbac11@gmail.com>
2023-04-09 02:36:30 -0500

    Final att

--- src/Server/Services/StatsService.lua
+++ src/Server/Services/StatsService.lua
@@ -3,10 +3,16 @@ local Players = game:GetService("Players")
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 -- local Promise = require(Knit.Util.Promise)
 
--- local Stats = {
--- 	"Level",
--- 	"Time",
--- }
+local Stats = {
+	{
+		Name = "Level",
+		Type = "Int",
+	},
+	{
+		Name = "Time",
+		Type = "Int",
+	},
+}
 
 local StatsService = Knit.CreateService({
 	Name = "StatsService",
@@ -17,6 +23,11 @@ local function OnPlayerAdded(player: Player)
 	local leaderstats = Instance.new("Folder")
 	leaderstats.Name = "leaderstats"
 	leaderstats.Parent = player
+
+	for _, stat in ipairs(Stats) do
+        local ins = Instance.new(stat.Type .. "Value")
+        ins.Parent = leaderstats
+	end
 end
 
 function StatsService:KnitStart() end
