7a202295d9233a455bb63dbfb615200a0ebdf3e3
Max Bacon <maxbac11@gmail.com>
2023-04-09 02:30:28 -0500

    Keep dirs

---
+++ src/Client/Controllers/UIController.lua
@@ -0,0 +1,9 @@
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+local UIController = Knit.CreateController({ Name = "UIController" })
+
+function UIController:KnitStart() end
+
+function UIController:KnitInit() end
+
+return UIController
