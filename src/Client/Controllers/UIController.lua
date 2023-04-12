ca638648dff4172e7ccd13acd952ad8b105e3bad
Max Bacon <maxbac11@gmail.com>
2023-04-11 23:18:54 -0500

    Add Bezier capabilities

--- src/Client/Controllers/UIController.lua
+++ src/Client/Controllers/UIController.lua
@@ -1,26 +1,40 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 local Knit = require(game:GetService("ReplicatedStorage").Utils.Knit)
 
-local Fusion = require(game:GetService("ReplicatedStorage").Utils.Fusion)
-local Value, Observer, Computed, ForKeys, ForValues, ForPairs = Fusion.Value, Fusion.Observer, Fusion.Computed, Fusion.ForKeys, Fusion.ForValues, Fusion.ForPairs
-local New, Children, OnEvent, OnChange, Out, Ref, Cleanup = Fusion.New, Fusion.Children, Fusion.OnEvent, Fusion.OnChange, Fusion.Out, Fusion.Ref, Fusion.Cleanup
-local Tween, Spring = Fusion.Tween, Fusion.Spring
+local Fusion = require(ReplicatedStorage.Utils.Fusion)
 
 local UIComponentsFolder = script.Parent.Parent.Modules.UIComponents
 
-local UIController = Knit.CreateController({ Name = "UIController" })
+local UIController = Knit.CreateController({ Name = "UIController", UIComponents = {} })
 
-local UIComponents = {}
+local New, Children, Value = Fusion.New, Fusion.Children, Fusion.Value
 
 function UIController:Get(name: string)
-    return UIComponents[name]
+	return self.UIComponents[name]
 end
 
-function UIController:KnitStart() end
+function UIController:KnitStart()
+    local StatsService = Knit.GetService("StatsService")
+    self.Gui = New("ScreenGui")({
+		Name = "Main",
+		Parent = game.Players.LocalPlayer.PlayerGui,
+
+        [Children] = {
+            self:Get("NewLevel")({
+                Event = StatsService.CheckpointReached
+            })
+        }
+	})
+end
 
 function UIController:KnitInit()
-    for _, mod in pairs(UIComponentsFolder) do
-        UIComponents[mod.Name] = require(mod)
-    end
+	self.GUI = Instance.new("ScreenGui")
+	self.GUI.Name = "MainGUI"
+
+	for _, mod in pairs(UIComponentsFolder:GetChildren()) do
+		self.UIComponents[mod.Name] = require(mod)
+	end
 end
 
 return UIController
---
+++ src/Client/Modules/UIComponents/NewLevel/init.lua
@@ -0,0 +1,36 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Fusion = require(ReplicatedStorage.Utils.Fusion)
+local BezierTween = require(ReplicatedStorage.Utils.BezierTween)
+local Images = require(ReplicatedStorage.Shared.Modules.Images)
+
+local Value, Observer, Computed, ForKeys, ForValues, ForPairs =
+	Fusion.Value, Fusion.Observer, Fusion.Computed, Fusion.ForKeys, Fusion.ForValues, Fusion.ForPairs
+local New, Children, OnEvent, OnChange, Out, Ref, Cleanup =
+	Fusion.New, Fusion.Children, Fusion.OnEvent, Fusion.OnChange, Fusion.Out, Fusion.Ref, Fusion.Cleanup
+local Tween, Spring = Fusion.Tween, Fusion.Spring
+
+local FlyingKatana = require(script.FlyingKatana)
+
+local function katanaAnimStarted()
+
+end
+
+local function newLevel(props)
+
+
+	local level = New("Frame")({
+		Name = "NewLevel",
+		BackgroundTransparency = 1,
+		Size = UDim2.fromScale(1,1),
+
+		[Children] = {
+			FlyingKatana(props)
+		}
+	})
+
+
+	return level
+end
+
+return newLevel
