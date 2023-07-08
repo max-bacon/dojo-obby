b34d89afcdd4cdb5b16890f33c6916043bc42639
Max Bacon <maxbac11@gmail.com>
2023-07-07 19:18:02 -0500

    test

--- BuildScripts/Win_BuildOffline.bat
+++ BuildScripts/Win_BuildOffline.bat
@@ -2,5 +2,5 @@
 title Lync Server
 
 cd %~dp0/..
-node "%LOCALAPPDATA%\Roblox\Lync\index.js" default.project.json OFFLINE
+node "%LOCALAPPDATA%\Roblox\Lync\index.js" default.project.json OFFLINE DEBUG
 pause
